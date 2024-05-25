79141fdc2b11bb09bab928d74bb4fb255cc33f30
katsiev1997 <katsiev1997@gmail.com>
2024-05-26 01:51:16 +0300

    верстка корзины

---
+++ src/components/CartItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full flex justify-between items-center">
+    <img src="" alt="" />
+    <div>
+      <h3 class="text-xl font-bold">Сырный цыпленок</h3>
+      <p class="text-lg text-gray-400">тонкое тесто, 26 см</p>
+    </div>
+    <div class="flex gap-3 flex-wrap">
+      <button
+        class="rounded-full text-center text-xl font-bold border border-orange-500 w-8 h-8"
+      >
+        -
+      </button>
+      <span class="text-xl font-bold">2</span>
+      <button
+        class="rounded-full text-center text-xl font-bold border border-orange-500 w-8 h-8"
+      >
+        +
+      </button>
+    </div>
+    <h3 class="text-xl font-bold">770 ₽</h3>
+    <button>
+      <img class="w-8 h-8" src="/delete.png" alt="x" />
+    </button>
+  </div>
+</template>
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,8 +1,38 @@
-<script setup>
-import Sort from "../components/Sort.vue";
-import Category from "../components/Category.vue";
+<script setup lang="ts">
+import CartItem from "../components/CartItem.vue";
 </script>
 
 <template>
-  <div></div>
+  <div class="w-full max-w-4xl m-auto flex flex-col gap-5">
+    <div class="flex justify-between w-full items-center">
+      <h1 class="text-2xl font-bold my-5 ml-4">Все пиццы</h1>
+      <button class="flex items-center gap-2 cursor-pointer">
+        <img src="/container.svg" alt="container" />
+        <p class="text-lg text-gray-400 hover:text-black">Очистить корзину</p>
+      </button>
+    </div>
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <div class="flex justify-between w-full items-center mt-10">
+      <p class="text-2xl">Всего пицц: <span class="font-bold">2 шт.</span></p>
+      <p class="text-2xl">
+        Сумма заказа: <span class="text-orange-500 font-bold">900 ₽</span>
+      </p>
+    </div>
+    <div class="flex justify-between w-full items-center my-10">
+      <button
+        class="rounded-full border border-gray-300 transition bg-white text-gray-300 p-5 hover:bg-orange-500 hover:text-white font-bold"
+      >
+        {{ "< Вернуться назад" }}
+      </button>
+      <button
+        class="rounded-full border border-gray-300 transition bg-orange-500 text-white p-5 hover:bg-white hover:text-orange-500 font-bold"
+      >
+        Оформить заказ
+      </button>
+    </div>
+  </div>
 </template>
